45e8de8b586a7ea4f316a46e659115e902e51a1d
dapplion <35266934+dapplion@users.noreply.github.com>
2026-02-05 23:22:07 +0100

    fix formatting

--- tests/core/pyspec/eth2spec/test/helpers/specs.py
+++ tests/core/pyspec/eth2spec/test/helpers/specs.py
@@ -17,9 +17,7 @@ ALL_EXECUTABLE_SPEC_NAMES = ALL_PHASES
 
 # import the spec for each fork and preset
 for fork in ALL_EXECUTABLE_SPEC_NAMES:
-    exec(
-        f"from eth2spec.{fork} import gnosis as spec_{fork}_gnosis"
-    )
+    exec(f"from eth2spec.{fork} import gnosis as spec_{fork}_gnosis")
 
 # this is the only output of this file
 spec_targets: Dict[PresetBaseName, Dict[SpecForkName, Spec]] = {
