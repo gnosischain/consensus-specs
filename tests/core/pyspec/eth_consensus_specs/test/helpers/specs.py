6f66e96cc5d6e87f10049809a0a30cd7579724ba
Chetna Miittal <chetna.mittal125@gmail.com>
2026-03-04 05:50:04 +0530

    chore: update specs

--- tests/core/pyspec/eth_consensus_specs/test/helpers/specs.py
+++ tests/core/pyspec/eth_consensus_specs/test/helpers/specs.py
@@ -1,25 +1,23 @@
-from typing import (
-    Dict,
-)
 from .constants import (
-    GNOSIS,
     ALL_PHASES,
+    GNOSIS,
 )
 from .typing import (
     PresetBaseName,
-    SpecForkName,
     Spec,
+    SpecForkName,
 )
 
-
-# NOTE: gnosis only
+# NOTE: Gnosis only
 ALL_EXECUTABLE_SPEC_NAMES = ALL_PHASES
 
 # import the spec for each fork and preset
 for fork in ALL_EXECUTABLE_SPEC_NAMES:
-    exec(f"from eth2spec.{fork} import gnosis as spec_{fork}_gnosis")
+    exec(
+        f"from eth_consensus_specs.{fork} import gnosis as spec_{fork}_gnosis"
+    )
 
 # this is the only output of this file
-spec_targets: Dict[PresetBaseName, Dict[SpecForkName, Spec]] = {
+spec_targets: dict[PresetBaseName, dict[SpecForkName, Spec]] = {
     GNOSIS: {fork: eval(f"spec_{fork}_gnosis") for fork in ALL_EXECUTABLE_SPEC_NAMES},
 }
